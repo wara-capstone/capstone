3472ad5cf3c6a397caa6df21150596737b01238b
changuii <rhljh7410@gmail.com>
2023-11-01 15:34:51 +0900

    ✅ 이미지 서버완료, 나중에 서버 주소, 포트 변경해야함

---
+++ imageServer/image/src/main/java/dev/image/image/service/ImageService.java
@@ -0,0 +1,17 @@
+package dev.image.image.service;
+
+
+import dev.image.image.dto.ImageDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public interface ImageService {
+    public ResponseEntity<ImageDTO> uploadImage(List<MultipartFile> images) throws IOException;
+    public ResponseEntity<byte[]> downloadImage(Long id);
+
+}
