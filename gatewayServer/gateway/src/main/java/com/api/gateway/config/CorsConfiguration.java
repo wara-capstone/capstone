0ad6339bf925455a94dcbc8feec3b6e74b768d18
changuii <rhljh7410@gmail.com>
2023-11-01 21:15:42 +0900

    ✅ gateway cors 처리

---
+++ gatewayServer/gateway/src/main/java/com/api/gateway/config/CorsConfiguration.java
@@ -0,0 +1,34 @@
+//package com.api.gateway.config;
+//
+//import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.reactive.config.CorsRegistry;
+//import org.springframework.web.reactive.config.WebFluxConfigurer;
+//
+//@Configuration
+//public class CorsConfiguration implements WebFluxConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowCredentials(true)
+//                .allowedOrigins("*")
+//                .allowedHeaders("*")
+//                .allowedMethods("*");
+//    }
+//
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.setAllowCredentials(true);
+//        corsConfiguration.addAllowedHeader("*");
+//        corsConfiguration.addAllowedMethod("*");
+//        corsConfiguration.addAllowedOrigin("*");
+//        corsConfiguration.addExposedHeader(HttpHeaders.SET_COOKIE);
+//        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+//        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+//        return new CorsWebFilter(corsConfigurationSource);
+//    }
+//}
