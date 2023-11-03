dda8a264b2f84e6ffa90e681567253d626a69be1
ryusemin <tpals3799@naver.com>
2023-11-04 03:30:10 +0900

    ✅ 유저프로필, 프로필수정 페이지 서버연결

--- front/practice/src/pages/Cart.jsx
+++ front/practice/src/pages/Cart.jsx
@@ -4,11 +4,20 @@ import BottomNav from "../components/BottomNav";
 import Card from "../components/Card"; // Card 컴포넌트 임포트
 import EventButton from "../components/EventButton";
 import Header from "../components/Header";
+import { useEffect } from "react";
 
 export default function Cart() {
   const CardInCart = Data.cardData.filter(
     (card) => card.isCartItems === Boolean(true)
   );
+
+    //
+    useEffect(() => {
+    }
+    , []);
+
+
+
 
   return (
     <div className="cart-page">
--- front/practice/src/pages/UserEdit.jsx
+++ front/practice/src/pages/UserEdit.jsx
@@ -1,27 +1,133 @@
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BottomNav from "../components/BottomNav";
 import EventButton from "../components/EventButton";
 import Header from "../components/Header";
 
 const UserEdit = ({ user }) => {
-  const [email, setEmail] = useState(user?.email || "");
-  const [name, setName] = useState(user?.name || "");
-  const [nickname, setNickname] = useState(user?.nickname || "");
-  const [phoneNumber, setPhoneNumber] = useState(user?.phone || "");
+  const token = sessionStorage.getItem("token");
+  const email = sessionStorage.getItem("email");
+
+  const [nickname, setNickname] = useState();
+  const [phoneNumber, setPhoneNumber] = useState();
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [image, setImage] = useState();
-  const [previewImageSrc, setPreviewImageSrc] = useState(
-    user?.profileImage || "https://via.placeholder.com/150x150"
-  );
+  var [image, setImage] = useState("");
+  const [previewImageSrc, setPreviewImageSrc] = useState("https://via.placeholder.com/150x150");
+
+  useEffect( () => {
+    console.log(token);
+    console.log("위에꺼 토큰");
+    const fetchData = async () => {
+    const response = await fetch(
+      'https://port-0-gateway-12fhqa2llofoaeip.sel5.cloudtype.app/user?email='+email,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `${token}`
+        },
+      }
+    );
+    const result = await response.json();
+    if (response.status === 200) {
+      setNickname(result.nickname);
+      setPhoneNumber(result.phone);
+      setImage(result.profileImage);
+      setPreviewImageSrc(result.profileImage);
+      console.log(result.profileImage);
+
+    } else {
+      console.log("실패");
+    }
+  };
+
+  fetchData();
+  }, []);
+
 
   // form submission handler
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // TODO: Implement the code to send updated data to the server
-    console.log({ email, name, nickname, phoneNumber, password, image });
+
+  // Check if passwords match
+  if (password !== passwordConfirm) {
+    alert("비밀번호가 일치하지 않습니다.");
+  return;
+  }
+
+    // Create payload
+    const payload = {
+      email: email,
+      nickname: nickname,
+      phone: phoneNumber,
+      password: password,
+    };
+
+    try {
+      const response = await fetch(
+        " https://port-0-gateway-12fhqa2llofoaeip.sel5.cloudtype.app/user",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `${token}`
+          },
+          body: JSON.stringify(payload),
+        }
+      );
+      const data = await response.json();
+      if (response.status === 200) {
+        // Redirect to login.html
+        console.log("성공!");
+      } else if (response.status === 400) {
+        // Handle error
+        alert(`실패`);
+      }
+    } catch (error) {
+      console.error("오류 발생:", error);
+    }
+
+
+
+    var formData = new FormData();
+        formData.append('image', image); // 이미지
+        console.log(formData);
+        // formData에 이미지와 json을 합친
+        for (let value of formData.values()) {
+            if (value instanceof Blob) {
+                var reader = new FileReader();
+                reader.onload = function () {
+                    console.log(reader.result); // Blob 내부 데이터를 콘솔에 출력
+                };
+                reader.readAsText(value);
+            } else {
+                console.log(value);
+            }
+        }
+        fetch('https://port-0-gateway-12fhqa2llofoaeip.sel5.cloudtype.app/user/image?email='+email, {
+            method: 'POST',
+            headers: {
+                "Authorization": `${token}`
+            },
+            body: formData
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json(); // JSON 형식의 응답을 파싱
+                }
+                throw new Error('네트워크 응답이 실패했습니다.');
+            })
+            .then(data => {
+                alert('성공!');
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
+
+    console.log({nickname, phoneNumber, password, image });
   };
 
   // image change handler
@@ -31,6 +137,7 @@ const UserEdit = ({ user }) => {
       setPreviewImageSrc(URL.createObjectURL(e.target.files[0]));
     }
   };
+
 
   return (
     <form onSubmit={handleSubmit} className="user-edit">
@@ -48,23 +155,6 @@ const UserEdit = ({ user }) => {
       </div>
 
       <div className="userEdit-info-container">
-        <label>이메일</label>
-        <div className="userEdit-email">
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-
-        <label>이름</label>
-        <div className="userEdit-name">
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
 
         <label>닉네임</label>
         <div className="userEdit-nickname">
@@ -88,7 +178,6 @@ const UserEdit = ({ user }) => {
         <div className="userEdit-password">
           <input
             type="password"
-            value={password}
             placeholder="새로운 비밀번호 입력"
             onChange={(e) => setPassword(e.target.value)}
           />
@@ -98,7 +187,6 @@ const UserEdit = ({ user }) => {
         <div className="userEdit-password-confirm">
           <input
             type="password"
-            value={passwordConfirm}
             placeholder="비밀번호 확인"
             onChange={(e) => setPasswordConfirm(e.target.value)}
           />
