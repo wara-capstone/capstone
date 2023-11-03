b0570cb889b64263fcddb6918cc280e0cbc4f99e
mimijae <qzqzcaraz00@gmail.com>
2023-11-03 22:58:31 +0900

    프론트와 꼬인 url 수정

--- chattingServer/chat/routing.py
+++ chattingServer/chat/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from chat import consumers
 
 websocket_urlpatterns = [
-    path("ws/chat/<int:room_id>/messages/", consumers.ChatConsumer.as_asgi()),
+    path("ws/chat/<int:room_id>/messages", consumers.ChatConsumer.as_asgi()),
 ]
 
