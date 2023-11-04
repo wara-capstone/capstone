91bd4523680ed3771a7cdf844caa8a947007db10
mimijae <qzqzcaraz00@gmail.com>
2023-11-05 02:39:10 +0900

    docker-compose 설정 코드 작성

--- chattingServer/config/asgi.py
+++ chattingServer/config/asgi.py
@@ -1,45 +1,29 @@
-"""
-ASGI config for mysite project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
 
 import os
+from django.core.asgi import get_asgi_application
 
-# channels에서 필요한 미들웨어와 라우팅 객체를 가져옵니다.
+# 환경변수 설정
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# Django ASGI 애플리케이션 초기화
+django_asgi_app = get_asgi_application()
+# from chat.views import register_service, deregister_service_from_eureka
+
+# register_service()
+
+# atexit.register(deregister_service_from_eureka)
+# channels 라우팅과 미들웨어는 Django 초기화 이후에 가져와야 합니다.
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-# Django의 기본 ASGI 어플리케이션 객체를 가져옵니다. (사실 여기서는 이제 사용되지 않음)
-from django.core.asgi import get_asgi_application
-import atexit
-from py_eureka_client import eureka_client
+import chat.routing  # 이제 이 코드는 안전하게 실행될 수 있습니다.
 
-# Django의 기본 설정 모듈을 지정합니다.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-from chat.views import register_service, deregister_service_from_eureka
-
-register_service()
-
-
-
-atexit.register(deregister_service_from_eureka)
-# chat 앱의 라우팅 설정을 가져옵니다.
-import chat.routing  # noqa: E402
-
-# 전체 ASGI 어플리케이션 구성을 정의합니다.
 application = ProtocolTypeRouter({
-    # 웹소켓 요청은 AllowedHostsOriginValidator로 유효성을 검사한 후,
-    # AuthMiddlewareStack을 거쳐 URLRouter로 라우팅됩니다.
-    "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
+    "http": django_asgi_app,
+    "websocket": 
         AuthMiddlewareStack(
             URLRouter(
                 chat.routing.websocket_urlpatterns
-            )
-        )
+            )     
     ),
 })
