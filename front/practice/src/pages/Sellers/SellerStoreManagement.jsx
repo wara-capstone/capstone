47f866d104c51942dc17ee506bfc0c649f17fb76
Jihu Kim <jihu0210@naver.com>
2023-11-09 03:22:16 +0900

    ✅상품관리, 상품등록 완료✅

--- front/practice/src/pages/Sellers/SellerStoreManagement.jsx
+++ front/practice/src/pages/Sellers/SellerStoreManagement.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
+import imageSrc4 from "../../adImages/iconImage/iconBlue.png";
+import imageSrc3 from "../../adImages/iconImage/iconRed.png";
+import imageSrc from "../../adImages/iconImage/shopBlue.png";
+import imageSrc2 from "../../adImages/iconImage/shopRed.png";
 import "./Seller.css";
 import SellerHeader from "./SellerHeader";
 import SellerSideNav from "./SellerSideNav";
@@ -11,6 +15,12 @@ const token = sessionStorage.getItem("token");
 var map;
 var geocoder;
 var marker;
+var markers = []; // 마커를 담을 배열
+var markerList = [],
+  selectedMarker = null; // 클릭한 마커를 담을 변수
+
+var imageSize = new kakao.maps.Size(42, 56); // 마커의 크기 기존 33, 36
+var choiceImageSize = new kakao.maps.Size(44, 58); // 선택한 마커의 크기 기존 38, 40
 
 const SellerStoreManagement = ({ store }) => {
   const [name, setName] = useState(store?.name || "");
@@ -27,6 +37,54 @@ const SellerStoreManagement = ({ store }) => {
     store?.profileImage || "https://via.placeholder.com/150x150"
   );
 
+  function createMarkerImage(markerScr, markerSize) {
+    var markerImage = new kakao.maps.MarkerImage(markerScr, markerSize);
+    return markerImage;
+  }
+
+  // 기본 마커이미지, 오버 마커이미지, 클릭 마커이미지를 생성합니다
+  var shopNormalImage = createMarkerImage(imageSrc, imageSize),
+    shopClickImage = createMarkerImage(imageSrc2, choiceImageSize),
+    clickImage = createMarkerImage(imageSrc3, choiceImageSize),
+    normalImage = createMarkerImage(imageSrc4, imageSize);
+
+  // fetch 통신 method
+  const fetchData = async (BodyJson, latlng, initMarkers) => {
+    try {
+      const response = await fetch(
+        "https://port-0-gateway-12fhqa2llofoaeip.sel5.cloudtype.app/store/read/map/coordinate",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: BodyJson,
+        }
+      );
+      const { data } = await response.json();
+      if (response.status === 200) {
+        // 서버에서 받은 데이터를 markerList에 저장
+        markerList = data;
+        console.log("데이터 전송 완료");
+        console.log(markerList);
+        console.log(latlng);
+        initMarkers();
+      } else if (response.status === 400) {
+        console.log("데이터 전송 실패");
+      }
+    } catch (error) {
+      console.error("오류 발생:", error);
+    }
+  };
+
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+
+  //
+  //
+  //
+  //
   useEffect(() => {
     var mapDiv = document.querySelector("#storeMap"), // 지도를 표시할 div
       mapOption = {
@@ -39,6 +97,130 @@ const SellerStoreManagement = ({ store }) => {
     }
 
     geocoder = new kakao.maps.services.Geocoder(); // 주소-좌표 변환 객체를 생성합니다
+
+    var circle = new kakao.maps.Circle({
+      center: mapOption.center,
+      radius: 10000,
+      strokeWeight: 5, // 선의 두께입니다
+      strokeColor: "#75B8FA", // 선의 색깔입니다
+      strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      strokeStyle: "dashed", // 선의 스타일 입니다
+      fillOpacity: 0, // 채우기 불투명도 입니다
+    });
+    var centerAround = circle.getBounds();
+    console.log(centerAround);
+
+    // centerAround의 남서쪽과 북동쪽 좌표를 가져옵니다
+    var swLatLng = centerAround.getSouthWest();
+    var neLatLng = centerAround.getNorthEast();
+
+    var circleXY = {
+      minX: swLatLng.getLng(), // 남서쪽 경도
+      minY: swLatLng.getLat(), // 남서쪽 위도
+      maxX: neLatLng.getLng(), // 북동쪽 경도
+      maxY: neLatLng.getLat(), // 북동쪽 위도
+    };
+
+    fetchData(JSON.stringify(circleXY), mapOption.center, initMarkers);
+    // 지도가 이동, 확대, 축소로 인해 중심좌표가 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+    kakao.maps.event.addListener(map, "dragend", function () {
+      // 지도의  레벨을 얻어옵니다
+      var level = map.getLevel();
+      // 지도의 중심좌표를 얻어옵니다
+      var latlng = map.getCenter();
+
+      circle.setPosition(latlng); // 지도의 중심좌표를 원의 중심으로 설정합니다
+      circle.setRadius(level * 1000); // 원의 반지름을 지도의 레벨 * 1000으로 설정합니다
+      circle.setMap(map); // 원을 지도에 표시합니다
+
+      var centerAround = circle.getBounds();
+      console.log(centerAround);
+
+      swLatLng = centerAround.getSouthWest();
+      neLatLng = centerAround.getNorthEast();
+
+      circleXY = {
+        minX: swLatLng.getLng(),
+        minY: swLatLng.getLat(),
+        maxX: neLatLng.getLng(),
+        maxY: neLatLng.getLat(),
+      };
+
+      console.log(circleXY);
+
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+      }
+
+      // markers 배열 초기화
+      markers = [];
+
+      var BodyJson = JSON.stringify(circleXY);
+      fetchData(BodyJson, latlng, initMarkers);
+    });
+
+    function initMarkers() {
+      if (markerList === null) {
+        console.log("데이터가 없습니다.");
+        return;
+      }
+      markerList.forEach(function (markerInfo) {
+        // 마커를 생성합니다
+
+        // if(markerInfo.Shopping){
+        // var marker = new kakao.maps.Marker({
+        //     position: new kakao.maps.LatLng( markerInfo.storeLocationY,markerInfo.storeLocationX),
+        //     map: map,
+        //     image: shopNormalImage, // 마커 이미지
+        // });
+        //     marker.normalImage = shopNormalImage;
+        //     marker.clickImage = shopClickImage;
+
+        // }else{
+        var marker = new kakao.maps.Marker({
+          position: new kakao.maps.LatLng(
+            markerInfo.storeLocationY,
+            markerInfo.storeLocationX
+          ),
+          map: map,
+          image: normalImage, // 마커 이미지
+        });
+        marker.normalImage = normalImage;
+        marker.clickImage = clickImage;
+        // }
+        markers.push(marker);
+
+        // 마커에 click 이벤트를 등록합니다
+        kakao.maps.event.addListener(marker, "click", function () {
+          // 클릭된 마커가 없거나, click 마커가 클릭된 마커가 아니면
+          // 마커의 이미지를 클릭 이미지로 변경합니다
+
+          if (!selectedMarker || selectedMarker !== marker) {
+            // 클릭된 마커 객체가 null이 아니면
+            // 클릭된 마커의 이미지를 기본 이미지로 변경하고
+            !!selectedMarker &&
+              selectedMarker.setImage(selectedMarker.normalImage);
+
+            // 현재 클릭된 마커의 이미지는 클릭 이미지로 변경합니다
+            marker.setImage(marker.clickImage);
+            // 클릭된 마커를 현재 클릭된 마커 객체로 설정합니다
+            selectedMarker = marker;
+          } else if (selectedMarker === marker) {
+            selectedMarker.setImage(selectedMarker.normalImage);
+            selectedMarker = null;
+          }
+
+          // 지도 중심을 클릭된 마커 위치로 이동
+          map.panTo(
+            new kakao.maps.LatLng(
+              markerInfo.storeLocationY,
+              markerInfo.storeLocationX
+            )
+          );
+        });
+      });
+    }
+    initMarkers();
   }, []);
 
   // form submission handler
@@ -137,6 +319,10 @@ const SellerStoreManagement = ({ store }) => {
       setPreviewImageSrc(URL.createObjectURL(e.target.files[0]));
     }
   };
+  //
+  //
+  //
+  //
 
   function searchAddress() {
     // 주소로 좌표를 검색합니다
