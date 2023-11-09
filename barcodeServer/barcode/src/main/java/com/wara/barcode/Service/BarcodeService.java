7203a9b8fd76161a09d14c43f2508022585538ca
kormk <63334787+kormk@users.noreply.github.com>
2023-11-09 20:33:19 +0900

    :white_check_mark: 코드정리

--- barcodeServer/barcode/src/main/java/com/wara/barcode/Service/BarcodeService.java
+++ barcodeServer/barcode/src/main/java/com/wara/barcode/Service/BarcodeService.java
@@ -28,37 +28,6 @@ public class BarcodeService {
 
     ObjectMapper objectMapper = new ObjectMapper();
 
-
-    /**
-     * dto의 id값으로 바코드를 구분/생성
-     */
-//    public byte[] createBarcode(BarcodeDTO dto) throws WriterException, IOException {
-//        // 바코드 사이즈
-//        int width = 800;
-//        int height = 200;
-//
-//        // TODO: 아래 try-catch에서 바코드에 등록할 정보 커스텀 해야함
-//        //바코드 이미지 생성
-//        try {
-//            // DTO를 Stirng으로 변환
-//            String url = objectMapper.writeValueAsString(dto);
-//
-//            // 바코드 정보 생성
-//            BitMatrix encode = new MultiFormatWriter().encode(url, BarcodeFormat.CODE_128, width, height);
-//            //output Stream
-//            ByteArrayOutputStream out = new ByteArrayOutputStream();
-//            //Bitmatrix, file.format, outputStream
-//            MatrixToImageWriter.writeToStream(encode, "PNG", out);
-//
-//            return out.toByteArray();
-//        }catch (NullPointerException e){ System.out.println(e.getMessage());}
-//
-//        return null;
-//    }
-
-
-
-
     public ByteArrayMultipartFile createBarcode(BarcodeDTO dto) throws WriterException, IOException {
         // 바코드 사이즈
         int width = 800;
