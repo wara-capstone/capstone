75c91041635867c76426d35ed8771d6a026b831a
2-jjong <jhyeon2450@gmail.com>
2023-12-02 17:13:26 +0900

    :white_check_mark: db schema 수정

--- storeServer/store/src/main/java/com/store/Service/HttpCommunicationServiceImpl.java
+++ storeServer/store/src/main/java/com/store/Service/HttpCommunicationServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
