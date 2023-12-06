c4cfe9e0d529e1c676c9cccfb7ae6a655e055a74
mimijae <qzqzcaraz00@gmail.com>
2023-12-07 01:00:04 +0900

    certbot 경로 수정

--- front/practice/init-letsencrypt.sh
+++ front/practice/init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(onoff.zone www.onoff.zone)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./practice/data/certbot"
 email="qzqzcaraz00@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
