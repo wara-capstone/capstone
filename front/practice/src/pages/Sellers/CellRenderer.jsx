e79df888d4b243aa16905ebf0ccfe3c4ca8e3a14
cozyrim <cofla0904@naver.com>
2023-12-07 00:07:19 +0900

    버튼 수정 중

--- front/practice/src/pages/Sellers/CellRenderer.jsx
+++ front/practice/src/pages/Sellers/CellRenderer.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
+import "./Seller.css";
 const CellRenderer = (props) => {
   const token = localStorage.getItem("token");
   const [storeInfo, setStoreInfo] = useState({ result: "", data: [] });
@@ -44,7 +44,9 @@ const CellRenderer = (props) => {
           // ,          state: { productId: productId },
         }}
       >
-        <button>등록/수정</button>
+        <button class="w-btn-outline w-btn-indigo-outline" type="button">
+        등록/수정
+    </button>
       </Link>
     </span>
   );
