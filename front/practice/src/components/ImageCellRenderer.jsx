43b6cc4a0a458db41fe9fafa63c0630a26275571
cozyrim <cofla0904@naver.com>
2023-12-07 18:07:34 +0900

    체크 박스 수정

--- front/practice/src/components/ImageCellRenderer.jsx
+++ front/practice/src/components/ImageCellRenderer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ImageCellRenderer = (props) => (
-  <img src={props.value[0]} style={{ width: '60px', height: 'auto', justifyContent: 'center', alignItems: 'center'}} alt="Product" />
+  <img src={props.value[0]} style={{ width: '60px'}} alt="Product" />
 );
 
 export default ImageCellRenderer;
