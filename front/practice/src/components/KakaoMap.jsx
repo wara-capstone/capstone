f999ec72a7fa4fbc0e382469bcea17b96c183a2b
ryusemin <tpals3799@naver.com>
2023-12-08 00:13:29 +0900

    ✅ 지도 범위표시 원 안보이게 수정

--- front/practice/src/components/KakaoMap.jsx
+++ front/practice/src/components/KakaoMap.jsx
@@ -228,7 +228,7 @@ export default function KakaoMap() {
         radius: 10000,
         strokeWeight: 5, // 선의 두께입니다
         strokeColor: "#75B8FA", // 선의 색깔입니다
-        strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+        strokeOpacity: 0, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
         strokeStyle: "dashed", // 선의 스타일 입니다
         fillOpacity: 0, // 채우기 불투명도 입니다
       });
