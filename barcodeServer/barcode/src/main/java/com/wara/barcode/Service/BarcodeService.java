77d6d89642aed08113be25a9788973f2c52d8b8a
kormk <63334787+kormk@users.noreply.github.com>
2023-12-09 01:07:49 +0900

    :white_check_mark: merge commit

--- barcodeServer/barcode/src/main/java/com/wara/barcode/Service/BarcodeService.java
+++ barcodeServer/barcode/src/main/java/com/wara/barcode/Service/BarcodeService.java
@@ -3,8 +3,6 @@ package com.wara.barcode.Service;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.simple.JSONObject;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
 import com.wara.barcode.DTO.BarcodeDTO;
 import com.google.zxing.common.BitMatrix;
@@ -12,16 +10,8 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.HashMap;
 
 
 @Service
