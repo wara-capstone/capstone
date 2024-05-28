e6c8003063cba7ef072338cd04131f1105b0d906
ryusemin <tpals3799@naver.com>
2024-05-29 02:05:25 +0900

    🐛 🐛

--- front/practice/src/pages/PurchaseRedirect.jsx
+++ front/practice/src/pages/PurchaseRedirect.jsx
@@ -10,6 +10,7 @@ const PurchaseRedirect = () => {
     // 로컬 스토리지에서 이메일과 토큰을 가져와 변수에 할당
     const email = localStorage.getItem("email");
     let token = localStorage.getItem("token");
+    const [update, setUpdate] = useState(false);
 
     // 각 쿼리 파라미터의 값을 변수에 할당
     const imp_uid = urlParams.get("imp_uid");
@@ -83,7 +84,8 @@ const PurchaseRedirect = () => {
                         if (response.status === 204) {
                           console.log("장바구니 삭제 성공");
                           localStorage.removeItem('checkList');
-                          navigate("/", { replace: true });
+                          navigate("/");
+                          setUpdate(true); // 상태 변경으로 리렌더링 강제
                         }
                          else {
                          console.log(response);
