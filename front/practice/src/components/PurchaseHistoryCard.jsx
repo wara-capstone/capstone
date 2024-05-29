66221f0805403e3eb34d619567b1974f2972c2f5
ryusemin <tpals3799@naver.com>
2024-05-30 03:34:34 +0900

    ✅ 결제내역 조회 수정 중 + moment 라이브러리 install

--- front/practice/src/components/PurchaseHistoryCard.jsx
+++ front/practice/src/components/PurchaseHistoryCard.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+
 import '../components/OrderProducts.css';
 
 export default function PurchaseHistoryCard({data}) {
@@ -6,22 +8,27 @@ export default function PurchaseHistoryCard({data}) {
     // const [quantity, setQuantity] = useState(selectedBread.product.quantity); //수량
     // const perPrice = parseInt (selectedBread.product.price); // Product당 가격 * 개수 값
 return (
+    <>
+    <h3>{moment.utc(data.dateTime).format('YYYY-MM-DD')}</h3>
+    {data.map((paymentDTO, index) => (
     <div className="orderProductList" style={{height:"7.5rem"}}> 
     <div style={{display:'flex',justifyContent:'space-around'}}>
      <div className="imageWrapper">
-     <img src={data.productUrls[0].url} alt="bread" className="breadImage" />
+     <img src={paymentDTO.productUrls[0].url} alt="bread" className="breadImage" />
     </div>
     <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-        <label style={{fontSize:"1.2rem"}}>{data.productName}</label>
+        <label style={{fontSize:"1.2rem"}}>{paymentDTO.productName}</label>
         <div style={{display: 'flex', justifyContent:"center"}} >
-        <span className="dayOption">{data.options[0].productColor + ", " + data.options[0].productSize}</span> 
+        <span className="dayOption">{paymentDTO.options[0].productColor + ", " + paymentDTO.options[0].productSize}</span> 
     </div>
-        <label style={{fontSize:"1.2rem"}}>{data.quantity} 개</label>
+        <label style={{fontSize:"1.2rem"}}>{paymentDTO.quantity} 개</label>
         </div>
     <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}} >
-        <h4>{(data.price * data.quantity).toLocaleString()}원</h4   > 
+        <h4>{(paymentDTO.price * paymentDTO.quantity).toLocaleString()}원</h4> 
     </div>
         </div>
 </div>
+   ))}
+</>
 );
 }
--- front/practice/src/pages/PurchaseHistory.jsx
+++ front/practice/src/pages/PurchaseHistory.jsx
@@ -4,7 +4,7 @@ import BottomNav from "../components/BottomNav";
 import Card from "../components/Card"; // Card 컴포넌트 임포트
 import EventButton from "../components/EventButton";
 import Header from "../components/Header";
-import { useEffect, useState} from "react";
+import { useEffect, useState ,useRef}  from "react";
 import { useLocation } from "react-router-dom";
 import PurchaseProduct from "../components/PurchaseProduct";
 import "../components/CartComponents.css";
@@ -16,7 +16,9 @@ import { fetchRefreshToken } from "../utils/authUtil";
 
 export default function PurchaseHistory() {
 
-    const [purchaseItems, setPurchaseItems] = useState ([]);
+    const [paymentItems, setPaymentItems] = useState ([]);
+    // const paymentItems = useRef([]);
+
     const navigate = useNavigate();
     const email = localStorage.getItem("email");
     const token = localStorage.getItem("token");
@@ -26,6 +28,7 @@ export default function PurchaseHistory() {
 
     // 총 금액
 //   const totalPrice = purchaseItems.reduce((acc, item) => acc + (item.product.price), 0);
+
 
 
   useEffect(() => {
@@ -54,14 +57,15 @@ export default function PurchaseHistory() {
 
           if(result.result === "success"){
             console.log("구매 내역이 존재합니다.");
-            result.data.map(data => {
+            result.data.forEach(async data => {
               setTotalPrice(totalPrice => totalPrice += data.totalPrice);
+
+
+              const purchaseItems = [];
 
               data.paymentDTOS.map(async payment => {
               let productId = payment.productId;
               let optionId = payment.optionId;
-
-
 
               let response2 =  await fetch(
                 `${process.env.NODE_ENV === 'development' ? '' : ''}${process.env.REACT_APP_API_URL}product/all/${productId}/option/${optionId}`,
@@ -77,12 +81,18 @@ export default function PurchaseHistory() {
                 const result = await response2.json();
                 result["quantity"] = payment.quantity;
                 result["price"] = payment.price;
-                setPurchaseItems(prevItems => [...prevItems, result]);
+                purchaseItems.push(result);
+
+
+                console.log("하나씩 조회");
+                console.log(purchaseItems)
               } else {
                 console.log("실패");
               }
-              }
-              );
+              });  
+
+              // paymentItems.current = [...paymentItems.current, purchaseItems];
+              setPaymentItems(prevItems => [...prevItems, purchaseItems]);
           });
           }
           else{
@@ -92,10 +102,10 @@ export default function PurchaseHistory() {
         } else{
           message.error("구매 내역을 불러오는데 실패하였습니다.", 1);
         }
-        console.log("구매내역", purchaseItems);
       };
       fetchData();
   }, [token]);
+
 
 
 
@@ -103,10 +113,13 @@ export default function PurchaseHistory() {
     <div className="cart-page">
       <Header />
       <div>
-        {purchaseItems.length > 0 ? ( 
+        {paymentItems.length > 0 ? ( 
             <div className="Cart">
-    {purchaseItems.map(data =>(
+    {paymentItems.map((data, index) =>(
+      console.log("데이터 확인"),
+      console.log([...data]),
         <PurchaseHistoryCard
+            key={index}
             data={data}
         />
         ))}
@@ -118,7 +131,7 @@ export default function PurchaseHistory() {
         <div style={{display: 'flex', justifyContent: 'space-between', padding: '20px'}}>
         <div>
           <label>구매 상품 개수: </label>
-          <span>{purchaseItems.length}</span>
+          <span>{paymentItems.length}</span>
         </div>
         <div>
           <label>총 구매 금액: </label>
