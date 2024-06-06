277410ecc92a141acd47a0b8d4e00be848e59c7f
cozyrim <cofla0904@naver.com>
2024-06-07 04:24:54 +0900

    댓글달기 구현

---
+++ front/practice/src/components/Comment.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import Button from '@mui/material/Button';
+import MoodIcon from '@mui/icons-material/Mood';
+
+
+function Comment({ onCommentSubmit }) {
+    const [commentText, setCommentText] = useState('');
+
+    const handleCommentTextChange = (event) => {
+      setCommentText(event.target.value);
+    };
+  
+    const handleCommentSubmit = () => {
+      // 여기에 실제 댓글 작성 로직을 구현합니다.
+      if (commentText.trim() !== '') {
+        onCommentSubmit(commentText.trim());
+        setCommentText('');
+      }
+    };
+
+
+    return (
+        <TextField
+          sx={{ paddingLeft: '1rem', paddingY: '0.5rem' }}
+          placeholder="댓글 달기..."
+          fullWidth
+          variant="standard"
+          value={commentText}
+          onChange={handleCommentTextChange}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <MoodIcon fontSize="large" />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment position="end">
+                <Button
+                  sx={{ fontWeight: 'bold', marginRight: '1rem'}}
+                  onClick={handleCommentSubmit}
+                >
+                  게시
+                </Button>
+              </InputAdornment>
+            ),
+            disableUnderline: true,
+          }}
+        />
+      );
+    };
+
+export default Comment;
