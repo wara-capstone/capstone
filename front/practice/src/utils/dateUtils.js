6035d9de7d63648fef8772b4a3f715455b26dc93
cozyrim <cofla0904@naver.com>
2024-06-07 10:02:42 +0900

    댓글 실시간

--- front/practice/src/utils/dateUtils.js
+++ front/practice/src/utils/dateUtils.js
@@ -1,15 +1,19 @@
 // utils/dateUtils.js
 export function formatCreatedAt(createdAt) {
-    const now = new Date();
-    const postDate = new Date(createdAt);
-    const diffInMinutes = Math.floor((now - postDate) / (1000 * 60));
+  const now = new Date();
+  const postDate = new Date(createdAt);
   
-    if (diffInMinutes < 60) {
-      return `${diffInMinutes}분 전`;
-    } else if (diffInMinutes < 1440) {
-      return `${Math.floor(diffInMinutes / 60)}시간 전`;
-    } else {
-      return `${Math.floor(diffInMinutes / 1440)}일 전`;
-    }
+  // 9시간 차이 보정
+  postDate.setHours(postDate.getHours() + 9);
+
+  const diffInMinutes = Math.floor((now - postDate) / (1000 * 60));
+
+  if (diffInMinutes < 60) {
+    return `${diffInMinutes}분 전`;
+  } else if (diffInMinutes < 1440) {
+    return `${Math.floor(diffInMinutes / 60)}시간 전`;
+  } else {
+    return `${Math.floor(diffInMinutes / 1440)}일 전`;
   }
+}
   
